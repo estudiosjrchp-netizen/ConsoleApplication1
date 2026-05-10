0e89e7fc92d9339612f3eb520d687a49ebbe04fe
TpSYCHO777 <estudiosjrchp@gmail.com>
2026-05-10 01:25:36 -0500

    cambios en la interfaz y el inicio de sesion

--- ConsoleApplication1/Controladora.h
+++ ConsoleApplication1/Controladora.h
@@ -14,13 +14,75 @@ public:
 	Controladora() {};
 	~Controladora() {};
 
-	
-    void menuRegistrarUsuario() {};
-    void menuEliminarUsuario() {};
+    void menuRegistrarUsuario() {
+        std::string nId, nNombre, nCorreo, nPass;
+        BorrarPantallaEscribible(); 
+        std::cout << "\033[38;2;146;208;80m"; //verde spotyfy
+        gotoxy(13, 32); std::cout << "--- REGISTRO DE USUARIO ---";
+        gotoxy(14, 32); std::cout << "Ingrese ID: "; std::cin >> nId;
+
+        // Verificamos si el ID ya existe antes de seguir
+        bool existe = false;
+        for (Usuario& u : listaUsuarios) {
+            if (u.getId() == nId) {
+                existe = true;
+                break;
+            }
+        }
+
+        if (existe) {
+            gotoxy(16, 37); std::cout << "Error: Ya existe un usuario con el ID " << nId;
+        }
+        else {
+            // Si no existe, pedimos el resto de datos
+            gotoxy(15, 32); std::cout << "Ingrese Nombre: "; std::cin >> nNombre;
+            gotoxy(16, 32); std::cout << "Ingrese Correo: "; std::cin >> nCorreo;
+            gotoxy(17, 32); std::cout << "Ingrese Contrasena: "; std::cin >> nPass;
+
+            // Creamos el usuario y lo metemos al vector
+            Usuario nuevoUsuario(nId, nNombre, nCorreo, nPass);
+            listaUsuarios.push_back(nuevoUsuario);
+
+            gotoxy(19, 32); std::cout << "Usuario registrado con exito!";
+        }
+        std::cout << "\033[0m";
+    }
+
+    void menuEliminarUsuario() {
+        std::string idEliminar;
+        BorrarPantallaEscribible();
+        std::cout << "\033[38;2;146;208;80m"; //verde spotyfy
+
+        gotoxy(13, 34); std::cout << "--- ELIMINAR USUARIO ---";
+        gotoxy(14, 34); std::cout << "Ingrese ID a eliminar: ";
+        std::cin >> idEliminar;
+
+        bool encontrado = false;
+
+        // Recorremos el vector buscando la posicion (i)
+        for (size_t i = 0; i < listaUsuarios.size(); i++) {
+            if (listaUsuarios[i].getId() == idEliminar) {
+                // Borramos el elemento en la posicion listaUsuarios.begin() + i
+                listaUsuarios.erase(listaUsuarios.begin() + i);
+                encontrado = true;
+
+                gotoxy(16, 34); std::cout << "Usuario eliminado correctamente.";
+                break;
+            }
+        }
+
+        if (!encontrado) {
+            gotoxy(16, 34); std::cout << "Error: No se encontro ningun usuario con ese ID.";
+        }
+        std::cout << "\033[0m";
+
+    }
+
     void menuIniciarSesion() {
         std::string idBuscado, passIngresada;
         BorrarPantallaEscribible();
-     
+        std::cout << "\033[38;2;146;208;80m"; //verde spotyfy
+
         gotoxy(13, 37); std::cout << "Ingrese ID: ";
         std::cin >> idBuscado;
 
@@ -30,14 +92,16 @@ public:
 
             if (usuarioActual.getId() == idBuscado) {
                 encontrado = true;
-                gotoxy(14, 37); std::cout << "Usuario encontrado. Ingrese contrasena: ";
+                gotoxy(14, 37); std::cout << "Usuario encontrado";
+                gotoxy(15, 37); std::cout << "Ingrese contrasena: ";
+                gotoxy(16, 37);
                 std::cin >> passIngresada;
 
-                if (usuarioActual.getContrasena() == passIngresada) {
-                    gotoxy(16, 37); std::cout << "¡Bienvenido, " << usuarioActual.getNombreUsuario() << "!";
+                if (usuarioActual.getContrasena() == passIngresada) { //aca adentro entraremos a otra parte del code con weas como las listas de usuario y cosas asi como para registrar cosas segun el usuario xd
+                    gotoxy(17, 37); std::cout << "Bienvenido, " << usuarioActual.getNombreUsuario() << "!";
                 }
                 else {
-                    gotoxy(16, 37); std::cout << "Contrasena incorrecta.";
+                    gotoxy(17, 37); std::cout << "Contrasena incorrecta.";
                 }
                 break; 
             }
@@ -46,6 +110,8 @@ public:
         if (!encontrado) {
             gotoxy(16, 37); std::cout << "El usuario no existe.";
         }
+        std::cout << "\033[0m";
+
     }
 
 
